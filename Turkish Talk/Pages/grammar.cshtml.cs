6f7780dab4549e02ce81984c662771983e2acaf7
idblow <79046531807@yandex.ru>
2024-03-10 19:08:06 +0300

    Грамматика, письмо, чтение - страницы

---
+++ Turkish Talk/Pages/grammar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Turkish_Talk.Pages
+{
+    public class grammarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- TurkishTalk.Persistance/ApplicationDBContext.cs
+++ TurkishTalk.Persistance/ApplicationDBContext.cs
@@ -1,11 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Identity.Client;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TurkishTalk.Persistance.Models;
+
 
 
 namespace TurkishTalk.Persistance
